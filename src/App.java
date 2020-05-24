4faa9770295e8947d1af71ff360be638d9700ee9
frankmwilsonm <frank.mamani@upeu.edu.pe>
2020-05-24 17:54:40 -0400

    commit from debian

--- src/App.java
+++ src/App.java
@@ -5,6 +5,6 @@ public class App {
         MainFrame m = new MainFrame();
         m.setVisible(true);
         // code del server
-        // Hi soy sullon
+        // Hi soy frank desde rpi
     }
 }
